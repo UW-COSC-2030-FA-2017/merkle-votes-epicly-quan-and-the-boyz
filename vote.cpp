ff0f7c3f9cbd9946be9f5e48b22a5f117e2bdfea
Jhoffm15 <31752679+Jhoffm15@users.noreply.github.com>
2017-12-02 00:16:02 -0700

    Update vote.cpp

--- vote.cpp
+++ vote.cpp
@@ -7,8 +7,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-
-	// Declaration of needed variables
 	ifstream fTwo;
 	ifstream fOne;
 	string fileName;
@@ -16,12 +14,27 @@ int main(int argc, char **argv)
 	string vote;
 	int time;
 
-	// Get file name from the user.
+	// select a hash
+	cout << "chose a hash to use" << endl;
+	cout << "1. James Hoffman's hash" << endl;
+	cout << "2. Damir Pulatov's hash " << endl;
+	cout << "3. Jesse Cornejo's hash" << endl;
+	cin >> hash;
+	if (hash <= 0 || hash >= 4)
+	{
+		cout << "sorry hash out of range, set to hash #1" << endl;
+		hash = 1;
+	}
+
+	pMT treeOne = pMT(hash);
+	pMT treeTwo = pMT(hash);
+
+	// ask for file name
 	cout << "file 1: ";
 	getline(cin, fileName);
 	fOne.open(fileName);
 
-	// If the file name is invalid, loop until valid.
+	//loop untill no fail
 	while (fOne.fail())
 	{
 		cout << "The file could not be opened." << endl;
@@ -29,23 +42,6 @@ int main(int argc, char **argv)
 		getline(cin, fileName);
 		fOne.open(fileName);
 	}
-
-	// Ask the user to select a hash function.
-	cout << "chose a hash to use" << endl;
-	cout << "1. James Hoffmans hash" << endl;
-	cout << "2. Damir Pulatov's hash " << endl;
-	cout << "3. Jesse Cornejo's hash" << endl;
-
-	// Get the user's input and pass it into the merkle tree constructor.
-	cin >> hash;
-	if (hash <=0 || hash >= 4)
-	{
-		cout << "sorry hash out of range, set to hash #1" << endl;
-		hash = 1;
-	}
-
-	pMT merkleTree = pMT(hash);
-	//pMT compTree = pMT(hash); //Make the compare tree's hash the same as the origial
 
 	// Keep track of number of votes = number of leaves
 	int number_nodes = 0;
@@ -56,34 +52,22 @@ int main(int argc, char **argv)
 	vector<string> votes;
 	vector<int> timestamps;
 
-	while (fTwo.eof() == false)
+	while (fOne.eof() == false)
 	{
-		fTwo >> vote;
+		fOne >> vote;
 		votes.push_back(vote);
 
-		fTwo >> time;
+		fOne >> time;
 		timestamps.push_back(time);
 
 		number_nodes++;
 	}
-
-	// Here we call function that will create an empty tree with number_nodes of leaves
-	// merkleTree.create_tree(number_nodes + 1);
-
-	// A 'for' loop to populate leaves with data
-	/*
-	for (int i = 0; i < number_nodes; i++)
-	{
-		merkleTree.insert(votes[i], timestamps[i]);	
-	}
-	*/
-
-	// merkleTree.insert(vote, time);
 	cout << number_nodes << endl;
-	//cout << merkleTree;
 
-	//Ask the user if they want to compare the first merkel tree
-	// to another tree and the file name
+	//close fOne
+	fOne.close();
+	
+	//get second file to read from
 	cout << "file 2: ";
 	getline(cin, fileName);
 	fTwo.open(fileName);
@@ -96,34 +80,31 @@ int main(int argc, char **argv)
 		fTwo.open(fileName);
 	}
 
-
-
 	// Reset number of nodes
 	number_nodes = 0;
 
 	// Clear votes and timestamps to reuse it 
 	votes.clear();
 	timestamps.clear();
+	getline(fTwo, line);
 	
 	//Build a new merkel tree 
 	while (fTwo.eof() == false)
 	{
-		fOne >> vote;
+		fTwo >> vote;
 		votes.push_back(vote);
 
-		fOne >> time;
+		fTwo >> time;
 		timestamps.push_back(time);
 
 		number_nodes++;
 	}
 	
-	// Close files
-	fOne.close();
+	// Close fTwo
 	fTwo.close();
 
-	// Compare trees
-	/*
-	if (merkleTree == compTree) 
+	// Compare treeOne and treeTwo
+	if (treeOne == treeTwo) 
 	{
 		cout << "Validated" << endl;
 	}
@@ -131,8 +112,7 @@ int main(int argc, char **argv)
 	{
 		cout << "Not Validatede" << endl;
 	}
-	*/
-	//If N was inputed exit program
-	fOne.close();
+	int a;
+	cin >> a;
 	return 0;
 }
